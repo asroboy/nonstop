5cd411d824ad5902d2630714d65e4111f4018d61
Ridho <asrofiridho@gmail.com>
2021-01-28 11:03:00 +0700

    change db

--- application/config/custom_config.php
+++ application/config/custom_config.php
@@ -2,13 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 date_default_timezone_set('Asia/Jakarta');
 $config['error_reporting'] =  error_reporting(0);
-$config['link_utama'] = 'https://demoteknologi.com/nonstop/';
-$config['link_path'] = 'https://demoteknologi.com/nonstop/';
+$config['link_utama'] = 'http://ordernonstop.com/nonstop/';
+$config['link_path'] = 'http://ordernonstop.com/nonstop/';
 
 $config['d_server'] = 'localhost';
-$config['d_user'] = 'wandemo_nonstop';
-$config['d_pw'] = '1qa2ws3ed!@#$%';
-$config['d_name'] = 'wandemo_nonstop';
+$config['d_user'] = 'admin';
+$config['d_pw'] = '#Tanpahenti24#';
+$config['d_name'] = 'nonstop';
 $config['d_debug'] = TRUE;
 
 $config['stat_admin'] = 'login_spadm_zxcvghjk_nonstop';
